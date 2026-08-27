642848b561514437702bcbe44b20d1232344d572
FREDDY <info@freddy.cn>
2026-08-27 05:22:17 +0200

    fix: FREDDY淮南分公司地址更新为中环国际广场158金融中心418室

--- projects/contracts/FREDDY-LIVANTO/generate_kooperation.py
+++ projects/contracts/FREDDY-LIVANTO/generate_kooperation.py
@@ -32,8 +32,8 @@ PARTIES = [
      '(1) LIVANTO 有限责任公司，地址：德国多特蒙德 Luisenhoffnung 3C（邮编 44225），于多特蒙德地方法院商业登记处登记（登记号 HRB 38628），增值税识别号 DE 464031041，由总经理钱子风代表 —— 作为委托方 ——'),
     ('– nachfolgend „LIVANTO“ genannt –,', '—— 以下称“LIVANTO”——，'),
     ('und', '与'),
-    ('(2) FREDDY (SHANGHAI) INFORMATION CONSULTING LTD. HN, Zweigniederlassung Huainan, Nr. 21, Longhu Road, Huainan, Anhui, Volksrepublik China, handelnd durch ihre Hauptniederlassung FREDDY (SHANGHAI) INFORMATION CONSULTING LTD., – als Auftragnehmerin –',
-     '(2) FREDDY (SHANGHAI) INFORMATION CONSULTING LTD. HN（福瑞笛（上海）信息咨询有限公司淮南分公司），地址：中华人民共和国安徽省淮南市龙湖路21号，由其总公司福瑞笛（上海）信息咨询有限公司授权行事 —— 作为受托方 ——'),
+    ('(2) FREDDY (SHANGHAI) INFORMATION CONSULTING LTD. HN, Zweigniederlassung Huainan, Zhonghuan International Plaza, Finanzzentrum 158, 4. Etage, Raum 418, Huainan, Anhui, Volksrepublik China, handelnd durch ihre Hauptniederlassung FREDDY (SHANGHAI) INFORMATION CONSULTING LTD., – als Auftragnehmerin –',
+     '(2) FREDDY (SHANGHAI) INFORMATION CONSULTING LTD. HN（福瑞笛（上海）信息咨询有限公司淮南分公司），地址：中华人民共和国安徽省淮南市中环国际广场158金融中心四层418室，由其总公司福瑞笛（上海）信息咨询有限公司授权行事 —— 作为受托方 ——'),
     ('– nachfolgend „FREDDY“ genannt –,', '—— 以下称“福瑞笛”——，'),
     ('– LIVANTO und FREDDY gemeinsam nachfolgend auch „Parteien“ genannt –', '—— LIVANTO 与福瑞笛以下合称“双方”——'),
 ]
